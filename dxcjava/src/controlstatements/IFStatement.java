4a1c7c20838d07d873ea92b68a284764a0495413
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-19 12:51:19 +0530

    if statement

---
+++ dxcjava/src/controlstatements/IFStatement.java
@@ -0,0 +1,11 @@
+package controlstatements;
+
+public class IFStatement {
+	public static void main(String[] args) {
+		int age=20;
+		
+		if(age>18){
+			System.out.print("Age is greater than 18");
+		}
+	}
+}
