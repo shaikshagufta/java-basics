2596cd0b432b0f78457069a2ba2a8efabd283422
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-19 13:56:59 +0530

    if else statement

---
+++ dxcjava/src/controlstatements/IfelseStatement.java
@@ -0,0 +1,14 @@
+package controlstatements;
+
+public class IfelseStatement {
+	public static void main(String[] args) {
+		int number=13;
+		
+		if(number%2==0){
+			System.out.println("even number");
+		}
+		else{
+			System.out.println("odd number");
+		}
+	}
+}
