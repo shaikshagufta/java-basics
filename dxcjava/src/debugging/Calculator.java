1a5567194fffee77b2f047221d6f8759ffa64cf0
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-21 00:11:38 +0530

    writing logs

--- dxcjava/src/debugging/Calculator.java
+++ dxcjava/src/debugging/Calculator.java
@@ -3,7 +3,11 @@ package debugging;
 public class Calculator {
 
 	public int add(int i, int j) {
-		return i+j;
+		System.out.println("Calculator-- add");
+		
+		int c = i+j;
+		System.out.println("Calculator-- add--"+c);
+		return c;
 	}
 	public int sub(int i, int j) {
 		return i-j;
--- dxcjava/src/debugging/Debug.java
+++ dxcjava/src/debugging/Debug.java
@@ -2,9 +2,12 @@ package debugging;
 
 public class Debug {
 	public static void main(String[] args) {
+		System.out.println("DebugDemo-- main");
 		Calculator calculator = new Calculator();
 		int sum = calculator.add(10, 20);
 		System.out.println("the sum is--"+sum);
+		
+		throw new NullPointerException();
 }
 
 }
