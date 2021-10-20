4fd431d03b49ba4326676a141d0dd399e22a4013
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-20 23:26:39 +0530

    debugging

--- dxcjava/src/debugging/Calculator.java
+++ dxcjava/src/debugging/Calculator.java
@@ -3,8 +3,15 @@ package debugging;
 public class Calculator {
 
 	public int add(int i, int j) {
-		// TODO Auto-generated method stub
 		return i+j;
 	}
-
+	public int sub(int i, int j) {
+		return i-j;
+	}
+	public int mul(int i, int j) {
+		return i*j;
+	}
+	public int div(int i, int j) {
+		return i/j;
+	}
 }
