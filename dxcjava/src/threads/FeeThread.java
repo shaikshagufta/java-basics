8fd8f46122f0a048770eb2c1b28ef310897f1a33
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-25 11:07:38 +0530

    threads

---
+++ dxcjava/src/threads/FeeThread.java
@@ -0,0 +1,8 @@
+package threads;
+
+public class FeeThread extends Thread{
+
+	public void run() {
+		System.out.println("got fee noc");
+	}
+}
---
+++ dxcjava/src/threads/NocProcess.java
@@ -0,0 +1,30 @@
+package threads;
+
+public class NocProcess {
+
+	public static void main(String[] args) {
+
+		oldSequentialWay();
+	}
+
+	private static void oldSequentialWay() {
+		try{
+			//fee
+		Thread.sleep(500);
+		System.out.println("got the fee no due stamp");
+		//hostel
+		Thread.sleep(500);
+		System.out.println("got the fee no due stamp");
+		//transport
+		Thread.sleep(500);
+		System.out.println("got the fee no due stamp");
+		//library
+		Thread.sleep(500);
+		System.out.println("got the fee no due stamp");
+    }
+	catch(Exception e) {
+    }
+	}
+}
+		
+
