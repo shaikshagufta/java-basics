4b844a333ab76ac967cee4c29e5add2ffd2ac349
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-26 12:18:48 +0530

    vector

---
+++ dxcjava/src/collections/VectorDemo.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+         Vector<String> names = new Vector<String>();
+         
+         names.add("shaggu");
+ 		names.add("shagufta");
+ 		Iterator<String> itr = names.iterator();
+ 		while (itr.hasNext());
+ 		{
+ 			System.out.println(itr.next());
+ 		}
+	}
+}
