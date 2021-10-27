bafcde34efb40e780e6b755d87a36db952fe2461
shaik <shaik@LAPTOP-GMIMKKPP>
2021-10-27 15:55:46 +0530

    testunits, setup and tear down

--- dxcjava/src/absractions/BMWFlyingCar.java
+++ dxcjava/src/absractions/BMWFlyingCar.java
@@ -5,19 +5,28 @@ public class BMWFlyingCar extends FlyingCar{
 	@Override
 	public void stopCar() {
 		System.out.println("switchoff propellors");
-		
+
 	}
 
 	@Override
 	public void flyCar() {
 		System.out.println("switch on the propellers");
-		
+
 	}
 
 	@Override
 	public void landCar() {
 		System.out.println("switch on landing gears");
-		
+	}
+
+
+	public static	int add(int a, int b) {
+		return a+b;
+	}
+
+	public static boolean isEligibletoVote(int age) {
+		if (age >18) {return true;}
+		return false;
 	}
 
 }
---
+++ dxcjava/src/absractions/BMWTest.java
@@ -0,0 +1,33 @@
+package absractions;
+
+import junit.extensions.TestSetup;
+import junit.framework.TestCase;
+
+public class BMWTest extends TestCase {
+	
+	public void testAdd() {
+		int expected = 30;
+		int actual = BMWFlyingCar.add(10, 20);
+		
+		assertEquals(expected, actual);
+	}
+	
+	public void testElgi() {
+		boolean actual = BMWFlyingCar.isEligibletoVote(17);
+		assertFalse(actual);
+	}
+	@Override
+protected void setUp() throws Exception { 
+	super.setUp();
+System.out.println("i am setting up the environment for test");
+
+	
+}
+
+@Override
+protected void tearDown() throws Exception {
+	// TODO Auto-generated method stub
+	super.tearDown();
+	System.out.println("i am tearing down the environment");
+}
+}
